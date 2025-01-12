870277380931f88f27e26505c416b950cca154f6
pavel <paveq2003@yandex.ru>
2025-01-12 23:49:45 +0300

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/5sezTyzoCq

---
+++ apps/sobes_life_server/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'sobes_life_server',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/sobes_life_server',
+};
